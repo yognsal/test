8d761a3cb31844b3d4d9485c168e8ef03aa384e8
yognsal <51479474+yognsal@users.noreply.github.com>
2024-10-08 16:17:48 +0530

    Add files via upload

---
+++ src/test/java/com/mnet/pojo/web/WebControl.java
@@ -0,0 +1,11 @@
+package com.mnet.pojo.web;
+
+import lombok.Data;
+
+// POJO to group all properties related to web control
+@Data
+public class WebControl {
+	
+	private String text;
+	private String textColor;
+}
